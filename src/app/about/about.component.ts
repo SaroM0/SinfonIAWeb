4faeb0f5433129f6e4445aed196ee80368949c2d
SaroM0 <s.rodriguezm2@uniandes.edu.co>
2023-11-17 08:22:11 -0500

    cambios varios

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent implements OnInit {
+export class AboutComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  ngAfterViewInit() {
+    $(function () {
+      // Tu código jQuery aquí
+      // Por ejemplo, puedes poner todo tu código dentro de este bloque
+      // $(window).on('scroll', function () { ... });
+      // $(window).on('resize', function () { ... });
+      // ...
+
+    });
+  }
 }
